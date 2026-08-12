de7af2811ae3bb2ce789536172cf82b3d2e4deb9
Elizandro Xavier <elizandroxavier268@gmail.com>
2026-08-12 15:46:04 +0100

    prj

--- script.js
+++ script.js
@@ -12,3 +12,14 @@ const botoes = document.querySelectorAll('.botaoComprar');
             btn.addEventListener('mouseenter', () => trocarTexto(btn, 'Comprar!', -1));
             btn.addEventListener('mouseleave', () => trocarTexto(btn, textoOriginal, 1));
         });
+
+const btnAbrirModal = document.getElementById('abrir-modal')
+const btnFecharModal = document.getElementById('fechar-modal')
+const modalBox = document.getElementById('modal-box')
+
+btnAbrirModal.addEventListener('click', function(){
+    modalBox.classList.add('aberto')
+})
+btnFecharModal.addEventListener('click', function(){
+    modalBox.classList.remove('aberto');
+})
